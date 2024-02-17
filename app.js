e64f01c96387941cc8ddf2b4e9168cde7808d8e6
X <x@Xs-MacBook-Pro.local>
2024-02-18 01:21:27 +0400

    Responsive Section

--- app.js
+++ app.js
@@ -54,5 +54,23 @@ function scrollUp(){
    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
     }
-    window.addEventListener ('scroll',scrollUp)
+    window.addEventListener ('scroll',scrollUp);
+
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+            sections.forEach(current =>{
+            const sectionHeight = current.offsetHeight,
+                    sectionTop = current.offsetTop - 58,
+                    sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        document.querySelector(' .nav menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }else{
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+  }
+})
+}
+window. addEventListener('scroll', scrollActive)
+
 
